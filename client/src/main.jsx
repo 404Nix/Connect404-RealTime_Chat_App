295deb8f1e1a122cf8ab83540baba2e96af4ae42
Nikhil <nikhilkanojia693@gmail.com>
2026-04-21 02:37:51 +0530

    fix: mui-icon fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { CssBaseline } from "@mui/material";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,7 +13,6 @@ const Login = lazy(() => import("./pages/Login.jsx"));
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <HelmetProvider>
-            <CssBaseline />
             <BrowserRouter>
                 <Suspense fallback={<div>Loading...</div>}>
                     <Routes>
